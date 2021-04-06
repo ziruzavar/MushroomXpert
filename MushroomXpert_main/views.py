0a45e4a05e50f1ee5e4d6470b3fc80bbe1c6aca5
ivan <ivan.vanir@gmail.com>
2021-04-06 21:39:48 +0300

    adding the project base

---
+++ MushroomXpert_main/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.views.generic.base import View
+
+
+class IndexView(View):
+    def get(self, request, **kwargs):
+        return render(request, 'index.html')
