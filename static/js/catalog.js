6cbd6929774281f0d75d2165bbfd3da35e599265
Dimitar.Petroww <Mitko66692@abv.bg>
2021-04-28 18:40:41 +0300

    Adding the Ai logic

--- static/js/catalog.js
+++ static/js/catalog.js
@@ -4,11 +4,14 @@ import {paginationTemplate} from "./templates/pagination.js"
 import {getMushes} from "./utils/getShroom.js"
 
 const page = window.location.search.split("=")[1] || 1
-const mushrooms = await getMushes()
+setup()
+async function setup() {
+    const mushrooms = await getMushes()
 
-const main = document.querySelector(".mushrooms")
-const paginator = document.querySelector(".pagination")
+    const main = document.querySelector(".mushrooms")
+    const paginator = document.querySelector(".pagination")
 
-const pages = Array.from({length: Math.ceil(mushrooms.length / 9)}, (v, i) => i + 1)
-render(mushroomTemplate(mushrooms.splice((page - 1) * 9, 9)), main)
-render(paginationTemplate(pages, page), paginator)
+    const pages = Array.from({length: Math.ceil(mushrooms.length / 9)}, (v, i) => i + 1)
+    render(mushroomTemplate(mushrooms.splice((page - 1) * 9, 9)), main)
+    render(paginationTemplate(pages, page), paginator)
+}
--- static/js/index.js
+++ static/js/index.js
@@ -5,15 +5,20 @@ import {getMush} from "./utils/getShroom.js"
 const shroom = window.location.search.split("=")[1]
 const main = document.getElementById("prediction")
 const staticPng = document.getElementById("static")
-if (shroom) {
-    main.style.display = "block"
-    staticPng.style.display = "none"
+setup()
 
-    const data = shroom.split("_")
-    const mushroom = await getMush(data[1] + "_" + data[2])
-    render(detailsTemplate(mushroom), main)
-} else {
-    main.style.display = "none"
-    staticPng.style.display = "block"
+async function setup() {
+    if (shroom) {
+        main.style.display = "block"
+        staticPng.style.display = "none"
+
+        const data = shroom.split("_")
+        const mushroom = await getMush(data[1] + "_" + data[2])
+        render(detailsTemplate(mushroom), main)
+    } else {
+        main.style.display = "none"
+        staticPng.style.display = "block"
+    }
 }
+
 Array.from(document.querySelectorAll(".form-detail > label")).map(x => x.remove())
