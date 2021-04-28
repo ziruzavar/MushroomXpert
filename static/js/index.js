c2df598853bb241a4ff835a609365c342ec4a43b
Dimitar.Petroww <Mitko66692@abv.bg>
2021-04-28 18:07:19 +0300

    Adding the Ai logic

--- static/js/index.js
+++ static/js/index.js
@@ -1,17 +1,19 @@
 import {render} from 'https://unpkg.com/lit-html?module';
-import { detailsTemplate } from "./templates/mushroomTemplate.js"
-import { getMush } from "./utils/getShroom.js"
-const shroom = window.location.search.split("=")[1]
-if(shroom) {
-    document.getElementById("prediction").style.display="block"
-    document.getElementById("static").style.display="none"
+import {detailsTemplate} from "./templates/mushroomTemplate.js"
+import {getMush} from "./utils/getShroom.js"
 
-    const main = document.querySelector("#prediction")
+const shroom = window.location.search.split("=")[1]
+const main = document.getElementById("prediction")
+const staticPng = document.getElementById("static")
+if (shroom) {
+    main.style.display = "block"
+    staticPng.style.display = "none"
+
     const data = shroom.split("_")
     const mushroom = await getMush(data[1] + "_" + data[2])
     render(detailsTemplate(mushroom), main)
-}else {
-    document.getElementById("prediction").style.display="none"
-    document.getElementById("static").style.display="block"
+} else {
+    main.style.display = "none"
+    staticPng.style.display = "block"
 }
-Array.from(document.querySelectorAll(".form-detail > label")).map(x=> x.remove())
+Array.from(document.querySelectorAll(".form-detail > label")).map(x => x.remove())
