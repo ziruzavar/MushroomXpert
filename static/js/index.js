388f9ac836861c89d5661f41af147658faa92a49
Dimitar.Petroww <Mitko66692@abv.bg>
2021-04-30 15:48:15 +0300

    Adding the Ai logic

--- static/js/index.js
+++ static/js/index.js
@@ -2,17 +2,17 @@ import {render} from 'https://unpkg.com/lit-html?module';
 import {detailsTemplate} from "./templates/mushroomTemplate.js"
 import {getMush} from "./utils/getShroom.js"
 
-const shroom = window.location.search.split("=")[1]
+const shroom = window.location.search.split("=")
 const main = document.getElementById("prediction")
 const staticPng = document.getElementById("static")
 setup()
 
 async function setup() {
-    if (shroom) {
+    if (shroom[1] && shroom[0] === "shroom") {
         main.style.display = "block"
         staticPng.style.display = "none"
 
-        const data = shroom.split("_")
+        const data = shroom[1].split("_")
         const mushroom = await getMush(data[1] + "_" + data[2])
         render(detailsTemplate(mushroom), main)
     } else {
